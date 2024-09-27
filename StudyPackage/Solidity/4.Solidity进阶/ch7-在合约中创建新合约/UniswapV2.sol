c635f049380dfd87b050505cc957780787728881
WJX2001 <1758807220@qq.com>
2024-09-27 14:37:27 +0800

    feat: 自毁合约

--- StudyPackage/Solidity/4.Solidity进阶/ch7-在合约中创建新合约/UniswapV2.sol
+++ StudyPackage/Solidity/4.Solidity进阶/ch7-在合约中创建新合约/UniswapV2.sol
@@ -7,10 +7,12 @@ contract Pair {
     address public token1; // 代币2
 
     constructor() payable {
+        // 将factory 赋值为工厂合约地址
         factory = msg.sender;
     }
 
     // called once by the factory at time of deployment
+    // 由工厂合约在部署完成后手动调用以初始化代币地址，将token0和token1更新为币对中两种代币的地址。
     function initialize(address _token0, address _token1) external {
         require(msg.sender == factory, "UniswapV2: FORBIDDEN"); // sufficient check
         token0 = _token0;
@@ -19,22 +21,13 @@ contract Pair {
 }
 
 // 工厂合约
-contract PairFactory2 {
+contract PairFactory{
     mapping(address => mapping(address => address)) public getPair; // 通过两个代币地址查Pair地址
     address[] public allPairs; // 保存所有Pair地址
 
-    function createPair2(
-        address tokenA,
-        address tokenB
-    ) external returns (address pairAddr) {
-        require(tokenA != tokenB, "IDENTICAL_ADDRESSES"); //避免tokenA和tokenB相同产生的冲突
-        // 用tokenA和tokenB地址计算salt
-        (address token0, address token1) = tokenA < tokenB
-            ? (tokenA, tokenB)
-            : (tokenB, tokenA); //将tokenA和tokenB按大小排序
-        bytes32 salt = keccak256(abi.encodePacked(token0, token1));
-        // 用create2部署新合约
-        Pair pair = new Pair{salt: salt}();
+    function createPair(address tokenA, address tokenB) external returns (address pairAddr) {
+        // 创建新合约
+        Pair pair = new Pair(); 
         // 调用新合约的initialize方法
         pair.initialize(tokenA, tokenB);
         // 更新地址map
