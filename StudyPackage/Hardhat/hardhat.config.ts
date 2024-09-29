a2cd0ed04db9065d850707fa9a913f18196c9799
WJX2001 <1758807220@qq.com>
2024-09-29 15:10:06 +0800

    feat: hardhat 调试

--- StudyPackage/Hardhat/hardhat.config.ts
+++ StudyPackage/Hardhat/hardhat.config.ts
@@ -1,8 +1,15 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
-
+import 'dotenv/config'
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
+  networks: {
+    hardhat: {},
+    sepolia: {
+      url: "https://sepolia.infura.io/v3/" + process.env.INFURA_ID,
+      accounts: [`0x${process.env.PRIVATE_KEY}`],
+    },
+  }
 };
 
 export default config;
